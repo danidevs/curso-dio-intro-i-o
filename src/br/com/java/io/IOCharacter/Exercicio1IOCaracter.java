c2a8762e348e93e12b1e10a25e8135e0f8481f6e
Daniel rodrigues <danielsk4te@hotmail.com>
2024-01-14 18:31:46 -0300

    exercicio 1

---
+++ src/br/com/java/io/IOCharacter/Exercicio1IOCaracter.java
@@ -0,0 +1,30 @@
+package br.com.java.io.IOCharacter;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Exercicio1IOCaracter {
+    public static void receberTecladoImprimirConsole() throws IOException{
+
+        System.out.println("Recomende 3 filmes:");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        do{
+            bw.write(line);
+            bw.newLine();
+            line = br.readLine();
+        }while (!(line.isEmpty()));
+        br.close();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        receberTecladoImprimirConsole();
+    }
+}
